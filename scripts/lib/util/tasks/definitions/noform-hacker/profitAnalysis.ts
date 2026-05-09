8376249e2beecaa7315033ad4644f43a20de7433
Stefan Valianu <749506+stefanvalianu@users.noreply.github.com>
2026-05-09 15:57:40 -0700

    Implement basic profit analysis

--- scripts/lib/util/tasks/definitions/noform-hacker/profitAnalysis.ts
+++ scripts/lib/util/tasks/definitions/noform-hacker/profitAnalysis.ts
@@ -2,16 +2,28 @@ import { NS } from "@ns";
 import { ServerInfo } from "../../../dashboardTypes";
 import { ServerAnalysis, ServerAnalysisReport } from "./info";
 
-// todo - this
 function analyzeTarget(ns: NS, server: ServerInfo): ServerAnalysis | undefined {
-  if (server.hostname === "foodnstuff") {
-    return {
-      hostname: server.hostname,
-      profitScore: 1,
-    };
-  }
+  // can't hack servers without admin rights, or our own servers
+  if (!server.hasAdminRights || server.purchasedByPlayer) return undefined;
 
-  return undefined;
+  // 0-1 value indicating our likelihood to succeed the hack
+  const hackChance = ns.hackAnalyzeChance(server.hostname);
+
+  // this is very imperfect as it depends on current state, but it tells us how much we'd gain from a hack
+  // formulas API allows us to do this more accurately
+  const hackProfit = ns.hackAnalyze(server.hostname);
+
+  const hackTime = ns.getHackTime(server.hostname);
+  const weakTime = ns.getWeakenTime(server.hostname);
+  const growTime = ns.getGrowTime(server.hostname);
+  const totalTime = hackTime + weakTime + weakTime + growTime;
+
+  const roughProfitPerSecond = hackChance * (hackProfit / totalTime);
+
+  return {
+    hostname: server.hostname,
+    profitScore: roughProfitPerSecond,
+  };
 }
 
 export function performAnalysis(ns: NS, servers: ServerInfo[]): ServerAnalysisReport {
